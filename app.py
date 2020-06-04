07ce8e06ecc027860a8db6e5f7b81bd77beaddfa
christopher-liu <christopherbrianliu@gmail.com>
2020-06-04 00:44:02 -0700

    Functional skeleton for webapp

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home_page():
     return render_template('index.html')
+
+
+@app.route('/prediction', methods = ['POST'])
+def prediction():
+
+    # Need to take information from POST request and convert that
+    # into features that the ML model can use. Then we render 
+    # index.html with prediction results passed in as data.
+    # Should define another function called featurizer that does
+    # the processing of POST data into features
+
+    # return render_template('index.html', pred_output = 'Price: ${}'.format(pred_price))
+
+    return render_template('index.html', pred_val = request.form['first'] )
